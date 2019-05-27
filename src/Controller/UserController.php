c585c483ad149a750b64816ee7232378e341e388
Yoann Bienvenu <ybienvenu@mail.welcome-int.xyz>
2019-05-27 09:21:13 +0200

    adding user update profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,6 +40,29 @@ class UserController extends AbstractController
     {
         $form = $this->createForm(UserType::class);
         $form->handleRequest($request);
+        return $this->render('user/create.html.twig', [
+            'user_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("user/update/{id}", name="user_update")
+     * @param $id
+     * @param Request $request
+     * @return RedirectResponse|Response
+     */
+    public function update($id, Request $request)
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()):
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            return $this->redirectToRoute('user_profile', ['id' => $id]);
+        endif;
+
         return $this->render('user/create.html.twig', [
             'user_form' => $form->createView(),
         ]);
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -34,7 +35,7 @@ class UserType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'E-mail'
             ])
-            ->add('avatar', FileType::class, [
+            ->add('avatar', TextType::class, [
                 'label' => 'Avatar',
                 'required' => false,
             ])
